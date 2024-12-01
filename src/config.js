b29680ea6ca45457cd1efd3446ee148fad2d99f5
pmanonline <petersonzoconis@gmail.com>
2024-12-01 13:42:25 +0100

    updated url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// const backendURL = "https://egroup-be.vercel.app";
-const backendURL = "http://localhost:3001";
+const backendURL = "https://egroup-be.vercel.app";
+// const backendURL = "http://localhost:3001";
 
 export default backendURL;
